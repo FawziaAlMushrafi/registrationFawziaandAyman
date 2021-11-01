ff8649b0524fbd5e14fdfde349a70c3b3b6c97c5
aymn <aymn.ali.ruh.k@tuwaiq.edu.sa>
2021-11-01 23:38:47 +0300

    app marh 7lo _Edit

--- app/src/main/java/com/example/registration/MainActivity.kt
+++ app/src/main/java/com/example/registration/MainActivity.kt
@@ -15,11 +15,11 @@ class MainActivity : AppCompatActivity() {
 
     }
     fun sineup(){
-        var name = binding.name.text.toString()
-        var pass1 = binding.editTextTextPassword1.text.toString()
-        var pass2 = binding.editTextTextPassword2.text.toString()
-        var email = binding.Email.text.toString()
-        var birth = binding.birth.text.toString()
+        var name = binding.nameEdit.text.toString()
+        var pass1 = binding.editTextTextPassword1Edit.text.toString()
+        var pass2 = binding.editTextTextPassword2Edit.text.toString()
+        var email = binding.emailEdit.text.toString()
+        var birth = binding.birthEdit.text.toString()
         if (name.isEmpty()||pass1.isEmpty()||pass2.isEmpty()||email.isEmpty()||birth.isEmpty()){
             binding.massage.text = "empty"
         }
